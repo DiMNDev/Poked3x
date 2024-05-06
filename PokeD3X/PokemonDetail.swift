6a9a7a3e15eb8811ebb9910d43de8c7a17bcddb9
DiMNDev <Dimndev@gmail.com>
2024-05-06 10:30:44 -0600

    Design Pokemon Detail View

---
+++ PokeD3X/PokemonDetail.swift
@@ -0,0 +1,67 @@
+//
+//  PokemonDetail.swift
+//  PokeD3X
+//
+//  Created by Joshua Arnold on 5/6/24.
+//
+
+import CoreData
+import SwiftUI
+
+struct PokemonDetail: View {
+    @EnvironmentObject var pokemon: Pokemon
+    @State var showShiny = false
+    var body: some View {
+        ScrollView {
+            ZStack {
+                Image("normalgrasselectricpoisonfairy")
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color: .black, radius: 6)
+
+                AsyncImage(url: showShiny ? pokemon.shiny : pokemon.sprite) { image in
+                    image.image?
+                        .resizable()
+                        .scaledToFit()
+                        .padding(.top, 50)
+                        .shadow(color: .black, radius: 10)
+                }
+            }
+
+            HStack {
+                ForEach(pokemon.types!, id: \.self) { type in
+                    Text(type.capitalized)
+                        .font(.title2)
+                        .shadow(color: .white, radius: 1)
+                        .padding([.top, .bottom], 7)
+                        .padding([.leading, .trailing])
+                        .background(Color(type.capitalized))
+                        .clipShape(.rect(cornerRadius: 50))
+                }
+                Spacer()
+            }
+            .padding()
+        }
+        .navigationTitle(pokemon.name!.capitalized)
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button {
+                    showShiny.toggle()
+                } label: {
+                    if showShiny {
+                        Image(systemName: "want.and.stars")
+                            .foregroundStyle(.yellow)
+                    } else {
+                        Image(systemName: "want.and.stars.inverse")
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+//        GetSamplePokemonFor(PokemonDetail())
+    PokemonDetail()
+        .environmentObject(SamplePokemon.samplePokemon)
+}
