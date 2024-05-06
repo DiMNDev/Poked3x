7f2cfba978acd1e527cfa6c3481dfd746e10d4d9
DiMNDev <Dimndev@gmail.com>
2024-05-06 09:51:57 -0600

    design Pokedex row / update to NavigationStack

--- PokeD3X/ContentView.swift
+++ PokeD3X/ContentView.swift
@@ -10,23 +10,42 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
-    
+
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Pokemon.id, ascending: true)],
         animation: .default)
     private var pokedex: FetchedResults<Pokemon>
-    
+
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(pokedex) { pokemon in
-                    NavigationLink {
-                        Text("\(pokemon.id): \(pokemon.name!.capitalized)")
-                    } label: {
-                        Text("\(pokemon.id): \(pokemon.name!.capitalized)")
+        NavigationStack {
+            List(pokedex) { pokemon in
+
+                NavigationLink(value: pokemon) {
+                    AsyncImage(url: pokemon.sprite) { image in
+                        image
+                            .resizable()
+                            .scaledToFit()
+                    } placeholder: {
+                        ProgressView()
                     }
+                    .frame(width: 100, height: 100)
+                    Text(pokemon.name!.capitalized)
                 }
             }
+            .navigationTitle("PokeDex")
+            .navigationDestination(for: Pokemon.self, destination: { pokemon in
+                VStack {
+                    AsyncImage(url: pokemon.sprite) { image in
+                        image
+                            .resizable()
+                            .scaledToFit()
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    .frame(width: .infinity)
+                    Text(pokemon.name!.capitalized)
+                }
+            })
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     EditButton()
@@ -35,6 +54,7 @@ struct ContentView: View {
         }
     }
 }
+
 #Preview {
     ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
 }
